d1939a7952ed6025e77505c90e2e283110571c7b
User244999 <147624050+notcore@users.noreply.github.com>
2025-05-08 02:21:03 +0700

    Update vercel-php to 0.7.0

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            // ssr: 'resources/js/ssr.tsx', // Komentari ini buat skip SSR
+            // ssr: 'resources/js/ssr.tsx', // Udah dikomentari buat skip SSR
             refresh: true,
         }),
         react(),
